bdac12ddaa8ae0c2130243293e5164428e5378c4
Alina <ms.sciurus@gmail.com>
2025-05-07 14:38:46 +0300

    vite conf changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/static/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // будет лежать как frontend/dist
+    assetsDir: '.', // ⚠️ положит index.js и index.css прямо в dist/
+    emptyOutDir: true,
+  },
 }));
