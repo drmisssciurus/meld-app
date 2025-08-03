57f650e0af9e1b191310b6e3f507ddd554060855
Alina <ms.sciurus@gmail.com>
2025-08-03 11:20:49 +0300

    added new ppublications

--- src/components/Publications/Publications.tsx
+++ src/components/Publications/Publications.tsx
@@ -12,7 +12,7 @@ function Publications({ sectionRef }: PublicationsProps) {
       <h2 className={styles.title}>Publications</h2>
       <div className={styles.wrapper}>
         <PublicationsItem
-          name={'Model’s architecture'}
+          name={'Model’s Architecture'}
           autors={
             'Martvel, G., Shimshoni, I. & Zamansky, A. Automated Detection of Cat Facial Landmarks. Int J Comput Vis (2024).'
           }
@@ -22,7 +22,7 @@ function Publications({ sectionRef }: PublicationsProps) {
           }
         />
         <PublicationsItem
-          name={'Dog landmark scheme'}
+          name={'Dog Landmark Scheme'}
           autors={
             'Martvel, G., Zamansky, A., Pedretti, G., Canori, C., Shimshoni, I. & Bremhorst A. (2025).'
           }
@@ -32,12 +32,44 @@ function Publications({ sectionRef }: PublicationsProps) {
           articleLink={'https://www.nature.com/articles/s41598-025-07040-3'}
         />
         <PublicationsItem
-          name={'Cat landmark scheme'}
+          name={'Cat Landmark Scheme'}
           autors={
             'Martvel, G., Farhat, N., Shimshoni, I., & Zamansky, A. (2023).'
           }
           articleName={'CatFLW: Cat Facial Landmarks in the Wild Dataset'}
           articleLink={'https://arxiv.org/abs/2305.04232'}
+        />
+        <PublicationsItem
+          name={'Video-based Pain Recognition in Cats'}
+          autors={
+            'Martvel, G., Lazebnik, T., Feighelstein, M., ... & Zamansky, A. (2025)'
+          }
+          articleName={
+            'Automated video-based pain recognition in cats using facial landmarks'
+          }
+          articleLink={'https://www.nature.com/articles/s41598-024-78406-2'}
+        />
+        <PublicationsItem
+          name={'Facial Dynamics of Brachycephalic Dogs'}
+          autors={
+            'Martvel, G., Eretová, P., Přibylová, L., ... & Zamansky, A. (2025).'
+          }
+          articleName={
+            'Continuous automated analysis of facial dynamics of brachycephalic and normocephalic dogs in different contexts'
+          }
+          articleLink={
+            'https://link.springer.com/article/10.1186/s12917-025-04839-0'
+          }
+        />
+        <PublicationsItem
+          name={'Cat Facial Signals as a Social Function'}
+          autors={
+            'Martvel, G., Scott, L., Florkiewicz, B., Zamansky, A., Shimshoni, I. & Lazebnik, T.'
+          }
+          articleName={
+            'Computational investigation of the social function of domestic cat facial signals'
+          }
+          articleLink={'https://www.nature.com/articles/s41598-024-79216-2'}
         />
       </div>
     </div>
